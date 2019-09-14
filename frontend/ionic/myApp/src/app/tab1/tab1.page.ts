32d0f7f1d84d8316fb4b75bf4f6127adfe577e2a
f0s3 <power.aerozoll@gmail.com>
2019-09-15 02:39:18 +0300

    Implement more final version of carousel

--- frontend/ionic/myApp/src/app/tab1/tab1.page.ts
+++ frontend/ionic/myApp/src/app/tab1/tab1.page.ts
@@ -6,7 +6,19 @@ import { Component } from '@angular/core';
   styleUrls: ['tab1.page.scss']
 })
 export class Tab1Page {
+  isNavOpened = false;
+  items = [
+    {
+      title: 'Log out',
+      url: '/auth/logout',
+      icon: './assets/logout.svg'
+    }
+  ];
 
   constructor() {}
+
+  myMethod(event: Event) {
+    this.isNavOpened = !this.isNavOpened;
+  }
 
 }
