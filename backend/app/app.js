ded00a30f14a78a99defda5faf46f0cd1525ffab
IlyaBielov <ibioriginalibi@gmail.com>
2019-09-14 12:36:13 +0300

    update

--- backend/app/app.js
+++ backend/app/app.js
@@ -3,4 +3,12 @@ const port = 3000;
 
 app.get('/', (req, res) => res.send('Hello World!'));
 
+app.post('/register', (req, res) => res.send('REGISTER'));
+
+app.get('/login', (req, res) => res.send('LOGIN'));
+
+app.get('/code', (req, res) => res.send('Add code'));
+
+app.put('/code', (req, res) => res.send('Share code'));
+
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));
